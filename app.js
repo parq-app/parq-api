570bda12de0d5c8e3024460157c7a27e1a29cc59
kenzshelley <kenzshelley@gmail.com>
2016-02-19 00:23:07 -0500

    Added spots controller skeleton w/ test geofire code.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.use(require('./controllers'));
+
 app.listen(8080, function () {
     console.log('Example app listening on port 3000!');
 });
+
