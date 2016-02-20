25fe33ee9d0e4dd21a1f6ab290388997513f19a6
kenzshelley <kenzshelley@gmail.com>
2016-02-20 14:36:33 -0500

    Add create/update routes for Spot and improve error handling.

--- controllers/spots.js
+++ controllers/spots.js
@@ -11,7 +11,32 @@ var geoFire = new GeoFire(firebaseRef);
 router.get('/:id', function(req, res) {
   Spot.get(req.params.id).then(function(spot) {
     res.json({spot: spot});
-  })
+  }).catch(error) {
+    res.status(500).json({error: error});  
+  }
 });
+
+// Create a new spot
+router.post('/', function(req, res) {
+  if (req.body.hasOwnProperty('userId') && req.body.hasOwnProperty('addr') && 
+     req.body.hasOwnProperty('title')) {
+     Spot.create(req.body.userId, req.body.addr, req.body.title).then(function(spot) {
+      res.json({spot: spot}); 
+     }).catch(function(error) {
+      res.status(500).json({error: error}); 
+     });
+  } else {
+    return false; /// Not the right way to handle this, need to return promise
+  }
+});
+
+// Update a spot
+router.put('/:id', function(req, res) {
+  Spot.update(req.params.id, req.body).then(function() {
+    res.status(204).send(); 
+  }).catch(function(error) {
+    res.status(500).json({error: error});
+  });
+})
 
 module.exports = router
--- models/spot.js
+++ models/spot.js
@@ -1,5 +1,6 @@
 var Firebase = require('firebase'),
-    GeoFire = require('geofire');
+    GeoFire = require('geofire'),
+    ModelPrototype = require('../models/model_prototype');
 
 var firebaseRef = new Firebase("https://parq.firebaseio.com/spots");
 
@@ -8,23 +9,26 @@ exports.create = function(userId, addr, title) {
   var spot = new Spot(userId, addr, title, null);
 
   return spotRef.set(spot.attributes).then(function() {
-    console.log("Successfully created spot!");
     spot.id = spotRef.key();
     return spot;
-  }, function(error) {
-    console.log("Error creating spot! " + error);
   });
 }
 
 exports.get = function(spotId) {
-  console.log(spotId);
   return firebaseRef.child(spotId).once("value").then(function(snapshot) {
     var spotData = snapshot.val();
     return new Spot(spotData.userId, spotData.addr, spotData.title, snapshot.key());
   });
 }
 
-function Spot(userId, addr, title, id) {
+exports.update = function(spotId, attrs) {
+  // Check that id is not null
+  if (spotId == null) return Promise.reject("Null spot ID");
+
+  return firebaseRef.child(spotId).update(attrs);    
+};
+
+exports.Spot = function Spot(userId, addr, title, id) {
   this.id = id;
   this.attributes = {
     userId: userId,
@@ -32,36 +36,5 @@ function Spot(userId, addr, title, id) {
     title: title
   }
 };
-
-Spot.prototype = {
-  // Update the object's attributes and persist to firebase.
-  update: function(changedAttrs) {
-    // Check that id is not null
-    if (!this.id) return false;
-    // validate attributes
-    for (var attr in changedAttrs) {
-      if (!this.attributes.hasOwnProperty(attr)) {
-        console.log("Invalid attribute: " + attr)
-        return false; 
-      }
-    }
-
-    return firebaseRef.child(this.id).update(changedAttrs).then(function() {
-      // Only actually modify local model after the change is successfully
-      // persisted to firebase. 
-      for (var attr in changedAttrs) {
-        this.attributes[attr] = changedAttrs[attr];
-      }
-    }, function(error) {});
-  },
-  getAttr: function(attr) {
-    return this.attributes[attr]; 
-  },
-  hasAttr: function(attr) {
-    return this.attributes.hasOwnProperty(attr);
-  } 
-};
-
-exports.Spot = Spot;
 
 module.exports = exports;
