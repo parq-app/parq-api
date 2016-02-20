65ddde01286652a4ed78a534ad02fa47af0c4a46
kenzshelley <kenzshelley@gmail.com>
2016-02-20 01:10:48 -0500

    Refactored spot model, implemented create/get spot.

--- controllers/spots.js
+++ controllers/spots.js
@@ -1,29 +1,15 @@
-"use strict";
-
 var express = require('express')
   , router = express.Router(), 
   Firebase = require('firebase'),
   GeoFire = require('geofire'),
   Spot = require('../models/spot');
 
+var firebaseRef = new Firebase("https://parq.firebaseio.com");
+var geoFire = new GeoFire(firebaseRef);
 
-// Sample Spot code:
-//var testSpot = new Spot("userid", "loc", "title");
-//console.log(testSpot);
-//testSpot.update({userId: "userid2"});
-//console.log(testSpot);
-//
-//testSpot.create().then(function() {
-//  console.log(testSpot);
-//  testSpot.update({userId: "userid3", title: "newTitle"});
-//  console.log(testSpot);
-//  testSpot.save().then(function() {
-//    console.log(testSpot);
-//  }, function(error) {});
-//}, function(error) {
-//});
-
-router.get('/', function(req, res) {
+// Get a spot
+router.get('/:id', function(req, res) {
 });
+
 
 module.exports = router
--- models/spot.js
+++ models/spot.js
@@ -1,20 +1,31 @@
 var Firebase = require('firebase'),
     GeoFire = require('geofire');
 
-exports.createSpot = function(userId, addr, title) {
+var firebaseRef = new Firebase("https://parq.firebaseio.com/spots");
 
+exports.create = function(userId, addr, title) {
+  var spotRef = firebaseRef.push();
+  var spot = new Spot(userId, addr, title, null);
+
+  return spotRef.set(spot.attributes).then(function() {
+    console.log("Successfully created spot!");
+    spot.id = spotRef.key();
+    return spot;
+  }, function(error) {
+    console.log("Error creating spot! " + error);
+  });
 }
 
 exports.get = function(spotId) {
-
+  console.log(spotId);
+  return firebaseRef.child(spotId).once("value").then(function(snapshot) {
+    var spotData = snapshot.val();
+    return new Spot(spotData.userId, spotData.addr, spotData.title, snapshot.key());
+  });
 }
 
-function Spot(userId, addr, title) {
-  this.id = null;
-  // Records local changes to the model
-  this.changes = {};
-  // Indicates whether or not the model has unsaved changes
-  this.isClean = true;
+function Spot(userId, addr, title, id) {
+  this.id = id;
   this.attributes = {
     userId: userId,
     addr: addr,
@@ -23,62 +34,34 @@ function Spot(userId, addr, title) {
 };
 
 Spot.prototype = {
-  firebaseRef: new Firebase("https://parq.firebaseio.com/spots"),
-  // Changes the model's internal representation -- does not persist to fb.
+  // Update the object's attributes and persist to firebase.
   update: function(changedAttrs) {
+    // Check that id is not null
+    if (!this.id) return false;
+    // validate attributes
     for (var attr in changedAttrs) {
-      // validate attributes
       if (!this.attributes.hasOwnProperty(attr)) {
         console.log("Invalid attribute: " + attr)
         return false; 
       }
+    }
 
-      // record the change
-      this.changes[attr] = [this.attributes[attr], changedAttrs[attr]];
-      this.attributes[attr] = changedAttrs[attr];
-    }
-    this.isClean = false;
-  },
-  // Persist any changes on the model to firebase.
-  save: function() {
-    if (Object.keys(this.changes).length == 0) return false;
-    var changedAttrs = {};
-    for (var attr in this.changes) {
-      changedAttrs[attr] = this.changes[attr][1];
-    }
-    var spotObj = this;
-    return this.firebaseRef.child(this.id).update(changedAttrs).then(function() {
-      spotObj.isClean = true; 
-      spotObj.changes = {}; 
+    return firebaseRef.child(this.id).update(changedAttrs).then(function() {
+      // Only actually modify local model after the change is successfully
+      // persisted to firebase. 
+      for (var attr in changedAttrs) {
+        this.attributes[attr] = changedAttrs[attr];
+      }
     }, function(error) {});
-  },
-  // If this object has not yet been created in fb, creates it.
-  create: function() {
-    var spotRef = this.firebaseRef.push()
-    var spotObj = this;
-    return spotRef.set(this.attributes).then(function() {
-      console.log("Successfully created spot!");
-      spotObj.id = spotRef.key();
-      spotObj.changes = {};
-      spotObj.isClean = true;
-      //var geoFire = new GeoFire(firebaseRef);
-      // Add to geofire
-      //geoFire.set("a_key", [20.0, 10.0]).then(function() {
-      //  console.log("yay"); 
-      //  res.send("Stored a location!")
-      //}, function(error) {
-      //  console.log("poop");
-      //});
-    }, function(error) {
-      console.log("Error creating spot!");
-    });
   },
   getAttr: function(attr) {
     return this.attributes[attr]; 
   },
   hasAttr: function(attr) {
-    return this.hasOwnProperty(attr);
+    return this.attributes.hasOwnProperty(attr);
   } 
 };
 
-module.exports = Spot;
+exports.Spot = Spot;
+
+module.exports = exports;
