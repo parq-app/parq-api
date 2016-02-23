f14cc97d2cda8646b67917646ecb5a83d2959a35
Matthew Grossman <matthewryangrossman@gmail.com>
2016-02-23 13:40:10 -0500

    Fixed whitespace issues

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -1,44 +1,44 @@
-var express = require('express'),
-    router = express.Router(),
-    Reservation = require('../models/reservation');
+var express = require('express');
+var Reservation = require('../models/reservation');
+
+var router = express.Router();
 
 // Create and reserve a new reservation
 router.post('/', function(req, res) {
-    Reservation.reserve(req.body.userId, req.body.latitude, req.body.longitude)
-        .then(function(reservation) {
-            res.json(reservation);
-        }).catch(function(error) {
-            res.status(500).json({error: error});
-        });
+  Reservation.reserve(req.body.userId, req.body.latitude, req.body.longitude)
+    .then(function(reservation) {
+      res.json(reservation);
+    }).catch(function(error) {
+      res.status(500).json({error: error});
+    });
 });
 
-// Occupy spot passed in the id 
+// Occupy spot passed in the id
 router.put("/:id/occupy", function(req, res) {
-    Reservation.occupy(req.params.id).then(function(reservation) {
-        console.log(reservation);
-        res.json(reservation);
-    }).catch(function(error) {
-        res.status(500).json({error: error});
-    })
+  Reservation.occupy(req.params.id).then(function(reservation) {
+    console.log(reservation);
+    res.json(reservation);
+  }).catch(function(error) {
+    res.status(500).json({error: error});
+  });
 });
 
-// Occupy spot passed in the id 
+// Occupy spot passed in the id
 router.put("/:id/finish", function(req, res) {
-    Reservation.finish(req.params.id).then(function(reservation) {
-        res.json(reservation);
-    }).catch(function(error) {
-        res.status(500).json({error: error});
-    })
-
+  Reservation.finish(req.params.id).then(function(reservation) {
+    res.json(reservation);
+  }).catch(function(error) {
+    res.status(500).json({error: error});
+  });
 });
 
 // Get a reservation by id
 router.get("/:id", function(req, res) {
-    Reservation.get(req.params.id).then(function(reservation) {
-        res.json(reservation);
-    }).catch(function(error) {
-        res.status(500).json({error: error});
-    });
+  Reservation.get(req.params.id).then(function(reservation) {
+    res.json(reservation);
+  }).catch(function(error) {
+    res.status(500).json({error: error});
+  });
 });
 
 module.exports = router;
--- controllers/spots.js
+++ controllers/spots.js
@@ -1,42 +1,40 @@
-var express = require('express'),
-  Firebase = require('firebase'),
-  router = express.Router(),
-  Spot = require('../models/spot');
+var express = require('express');
+var Spot = require('../models/spot');
 
-var firebaseRef = new Firebase("https://parq.firebaseio.com");
+var router = express.Router();
 
 // Get a spot
 router.get('/:id', function(req, res) {
   Spot.get(req.params.id).then(function(spot) {
     res.status(200).json({spot: spot});
   }).catch(function(error) {
-    res.status(500).json({error: error});  
+    res.status(500).json({error: error});
   });
 });
 
 // Create a new spot
 router.post('/', function(req, res) {
-  if (req.body.hasOwnProperty('userId') && req.body.hasOwnProperty('addr') && 
-     req.body.hasOwnProperty('title') && req.body.hasOwnProperty('lat') && 
+  if (req.body.hasOwnProperty('userId') && req.body.hasOwnProperty('addr') &&
+     req.body.hasOwnProperty('title') && req.body.hasOwnProperty('lat') &&
      req.body.hasOwnProperty('long')) {
-     Spot.create(req.body.userId, req.body.addr, req.body.lat, req.body.long, req.body.title).
-     then(function(spot) {
-      res.json({spot: spot}); 
+    Spot.create(req.body.userId, req.body.addr, req.body.lat, req.body.long, req.body.title)
+     .then(function(spot) {
+       res.json({spot: spot});
      }).catch(function(error) {
-      res.status(500).json({error: error}); 
+       res.status(500).json({error: error});
      });
   } else {
-    res.status(400).json({error: "Missing expected body parameter."})
+    res.status(400).json({error: "Missing expected body parameter."});
   }
 });
 
 // Update a spot
 router.put('/:id', function(req, res) {
   Spot.update(req.params.id, req.body).then(function() {
-    res.status(204).send(); 
+    res.status(204).send();
   }).catch(function(error) {
     res.status(500).json({error: error});
   });
-})
+});
 
 module.exports = router;
