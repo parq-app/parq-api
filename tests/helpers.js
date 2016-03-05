0d1043b63642b448b80d10be4f72fec09dd997e4
Matthew Grossman <matthewryangrossman@gmail.com>
2016-03-05 18:33:30 -0500

    Added testing files. Some aren't passing, but the tests are right

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,6 +1,44 @@
 var Firebase = require('Firebase');
+var Spot = require('../models/spot');
+var User = require('../models/user');
 
-var usersRef = new Firebase("https://parq.firebaseio.com/users");
+var firebaseRef = new Firebase('https://parq.firebaseio.com');
+var usersRef = firebaseRef.child("users");
+
+var users = [
+  {email: "mrgrossm@umich.edu", password: "mrgrossm", id: ""},
+  {email: "nickmorg@umich.edu", password: "nickmorg", id: ""},
+  {email: "kenzshelley@umich.edu", password: "kenzshelley", id: ""}
+];
+
+var removeUsers = function() {
+  return Promise.all(users.map(function(user) {
+    return firebaseRef.removeUser({email: user.email, password: user.password});
+  })).catch(function(err) {});
+};
+
+var addUsers = function() {
+  return Promise.all(users.map(function(user) {
+    return User.create(user.email, user.password);
+  })).catch(function(err) {});
+};
+
+var addSpots = function() {
+  return Promise.all([
+    Spot.create(users[0].id,
+      "1111 South Forest Ave., Ann Arbor, MI 48104",
+      42.268629, -83.732678,
+      "Matt's house"),
+    Spot.create(users[1].id,
+      "916 Mary St., Ann Arbor, MI 48104",
+      42.270427, -83.742233,
+      "Nick's House"),
+    Spot.create(users[2].id,
+      "210 North Thayer St., Ann Arbor, MI 48104",
+      42.282546, -83.739348,
+      "Kenz's House")
+  ]);
+};
 
 exports.getUsers = function() {
   return usersRef.once("value").then(function(snapshot) {
@@ -13,4 +51,17 @@ exports.getUsers = function() {
     }
     return idArray;
   });
+};
+
+exports.refreshDbWithOneSpotEach = function() {
+  return firebaseRef.remove()
+    .then(removeUsers)
+    .then(addUsers)
+    .then(function(results) {
+      for (var i = 0; i < users.length; i++) {
+        users[i].id = results[i].id;
+      }
+    })
+    .then(addSpots)
+    .then(exports.getUsers);
 };
--- tests/models/reservation.js
+++ tests/models/reservation.js
@@ -1,23 +1,58 @@
-var Reservation = require('../../models/reservation');
+var assert = require('assert');
+
 var Helpers = require('../helpers');
 
-var chipotle = {lat: 42.279247, long: -83.740605};
+var Reservation = require('../../models/reservation');
+var Spot = require('../../models/spot');
+var Loc = require('../../models/loc');
 
-var users = [];
-Helpers.getUsers()
-  .then(function(userVals) {
-    users = userVals;
-  })
-  .then(function() {
-  // must do in this function to ensure it isn't run until users is populated
-    return Reservation.reserve(users[2], chipotle.lat, chipotle.long);
-  })
-  .then(function(reservation) {
-    return Reservation.get(reservation.id);
-  })
-  .then(function(reservation) {
-    console.log("Successful reservation get: " + JSON.stringify(reservation));
-  })
-  .catch(function(error) {
-    console.log("Failed reservation get: " + error);
+var chipotle = {lat: 42.279247, long: -83.740605};
+describe('Reservation', function() {
+  var users = [];
+  this.timeout(15000);
+  before(function(done) {
+    Helpers.refreshDbWithOneSpotEach().then(function(u) {
+      users = u;
+      done();
+    }).catch(function(error) {
+      done(error);
+    });
   });
+
+  describe('#reserve()', function() {
+    var reservation;
+
+    it('creates a new reservation and checks attrs', function(done) {
+      Reservation.reserve(users[0], chipotle.lat, chipotle.long)
+        .then(function(res) {
+          reservation = res;
+          assert.equal(reservation.hostId, users[0].id);
+          assert.equal(reservation.attributes.status, "reserved");
+          done();
+        }).catch(function(error) {
+          done(error);
+        });
+    });
+
+    it('checks that the new reservation spot is reserved', function(done) {
+      Spot.get(reservation.attributes.spotId)
+        .then(function(spot) {
+          assert.equal(spot.id, reservation.attributes.spotId);
+          assert(spot.attributes.isReserved);
+          done();
+        }).catch(function(error) {
+          done(error);
+        });
+    });
+
+    it('checks that the reserved loc is not in the free list', function(done) {
+      Loc.getAllLocs()
+        .then(function(locs) {
+          assert(!locs.hasOwnProperty(reservation.geohash));
+          done();
+        }).catch(function(error) {
+          done(error);
+        });
+    });
+  });
+});
