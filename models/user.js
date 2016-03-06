a1907e8e147526d19b30c01fa882729b45919a2e
Matthew Grossman <matthewryangrossman@gmail.com>
2016-03-05 19:16:40 -0500

    Fixes attribute issue with User
    Closes #17

--- models/user.js
+++ models/user.js
@@ -16,7 +16,12 @@ exports.create = function(email, password) {
 exports.get = function(id) {
   return firebaseRef.child(id).once("value").then(function(snapshot) {
     var userData = snapshot.val();
-    return new User(userData.email, snapshot.key());
+    var user = new User(userData.email, snapshot.key());
+
+    // fix ctor to do this basically
+    user.id = snapshot.key();
+    user.attributes = snapshot.val();
+    return user;
   });
 };
 
